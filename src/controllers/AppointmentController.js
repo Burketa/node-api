e860c52531e32fd8ba16b81ab18fcf36d6fa0b1b
burca <rlpelizzon@hotmail.com>
2019-09-25 10:16:19 -0300

    crud basico completo

--- src/controllers/AppointmentController.js
+++ src/controllers/AppointmentController.js
@@ -2,14 +2,37 @@ const mongoose = require("mongoose");
 const Appointment = mongoose.model("Appointment");
 
 module.exports = {
-  async index(req, res) {
+  async showAll(req, res) {
     const appointments = await Appointment.find();
 
     return res.json(appointments);
   },
 
-  async store(req, res) {
+  async read(req, res) {
+    const appointment = await Appointment.findById(req.params.id);
+
+    return res.json(appointment);
+  },
+
+  async create(req, res) {
     const appointment = await Appointment.create(req.body);
     return res.json(appointment);
+  },
+
+  async update(req, res) {
+    const appointment = await Appointment.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    return res.json(appointment);
+  },
+
+  async delete(req, res) {
+    await Appointment.findByIdAndRemove(req.params.id);
+
+    return res.send();
+    //const appointment = await Appointment.create(req.body);
+    //return res.json(appointment);
   }
 };
--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,11 @@ const routes = express.Router();
 const AppointmentController = require("./controllers/AppointmentController");
 
 //Rotas
-routes.get("/appointments", AppointmentController.index);
-routes.post("/appointments", AppointmentController.store);
+routes.get("/appointments", AppointmentController.showAll);
+routes.post("/appointments", AppointmentController.create);
+routes.get("/appointments/:id", AppointmentController.read);
+routes.put("/appointments/:id", AppointmentController.update);
+routes.delete("/appointments/:id", AppointmentController.delete);
 
 //Exportando as rotas para serem usadas no server.js
 module.exports = routes;
